86d9faea99c568ed4f70a2e6d0bdccfb343d9deb
maunavarro19 <maunavarro019@gmail.com>
2025-08-22 15:34:12 -0500

    Update app (5).py

--- app (5).py
+++ app (5).py
@@ -6,14 +6,12 @@ data = pd.read_csv('munis (1).csv')
 
 st.title("Primera aplicación ")
 
-st.dataframe(data)
-
 munis = data['entidad'].unique().tolist()
 mun = st.selectbox('seleccione un municipio: ',
              munis)
              
 filtro = data[data['entidad'] ==mun]
-st.dataframe(data)
+
 
 gen = (filtro 
        .groupby('clasificacion_ofpuj')['total_recaudo']
@@ -25,9 +23,7 @@ det = ( filtro
        .groupby('clasificacion_ofpuj')['total_recaudo']
        .sum())
 
-st.dataframe(gen) # clasificacion general
 
-st.dataframe(det) # clasificacion detallada 
 
 #pie chart
 fig, ax = plt.subplots(1, 1, figsize=(10, 6))
@@ -38,12 +34,13 @@ fin = (filtro
        .groupby(['clas_gen', 'clasificacion_ofpuj'])['total_recaudo'] 
        .sum()
        .reset_index())
-st.dataframe(fin)
+
 fig = px.treemap(fin, path=[px.Constant('Total'), 
                             'clas_gen',
                             'clasificacion_ofpuj'],
                             values='total_recaudo')
 st.plotly_chart(fig)
+
 
 
 
