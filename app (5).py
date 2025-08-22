3aef5f438d73ac6e88b3a76e4fe830e52c40fae9
maunavarro19 <maunavarro019@gmail.com>
2025-08-22 15:19:02 -0500

    Update app (5).py

--- app (5).py
+++ app (5).py
@@ -8,7 +8,7 @@ st.title("Primera aplicación ")
 
 st.dataframe(data)
 
-muinis = data['entidad'].unique().tolist()
+muinis (1) = data['entidad'].unique().tolist()
 mun = st.selectbox('seleccione un municipio: ',
              munis)
              
@@ -44,6 +44,7 @@ fig = px.treemap(fin, path=[px.Constant('Total'),
                             'clasificacion_ofpuj'],
                             values='total_recaudo')
 st.plotly_chart(fig)
+
 
 
 
